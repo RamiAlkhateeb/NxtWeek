450d38e60ab863e7de3925bc9750e10930cc5062
Rami Alkhateeb <38619924+RamiAlkhateeb@users.noreply.github.com>
2026-07-16 12:03:57 +0200

    Add Household model with properties for planning

---
+++ MealPlanner.Shared/Models/Household.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace MealPlanner.Shared.Models;
+
+public class Household
+{
+    public string Id { get; set; } = string.Empty;
+    public List<string> MemberUsernames { get; set; } = new();
+    public Dictionary<string, WeeklyPlanEntry> WeeklyPlan { get; set; } = new();
+    public List<string> FavoriteMealIds { get; set; } = new();
+}
